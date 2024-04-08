e55a46b590b8702d1d8357e196a8cbe7e9230149
kimutaieddy <131693884+kimutaieddy@users.noreply.github.com>
2024-04-08 19:58:23 +0300

    django admin page

--- weather/admin.py
+++ weather/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
 
 # Register your models here.
+# In weather/admin.py
+
+from django.contrib import admin
+from .models import Location, WeatherData
+
+# Register your models here.
+admin.site.register(Location)
+admin.site.register(WeatherData)
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
 # Create your views here.
+# In weather/views.py
+
+from django.shortcuts import render
+from .models import Location, WeatherData
+
+def index(request):
+    # Example view to display weather information
+    locations = Location.objects.all()
+    context = {
+        'locations': locations,
+    }
+    return render(request, 'weather/index.html', context)
